5702678bd4975968bdd0eb7d3a66494930b7bb18
Vlad <3dvlad2013@gmail.com>
2020-07-16 15:27:56 +0300

    Init

---
+++ device/gome/u7/AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_u7.mk
+COMMON_LUNCH_CHOICES:=\
+    full_u7-eng \
+    full_u7-userdebug \
+    full_u7-user
---
+++ device/gome/u7/BoardConfig.mk
@@ -0,0 +1,36 @@
+# Use the non-open-source part, if present
+-include vendor/mediatek/u7/BoardConfigVendor.mk
+
+# Use the 6757 common part
+include device/mediatek/mt6757/BoardConfig.mk
+
+MTK_INTERNAL_CDEFS := $(foreach t,$(AUTO_ADD_GLOBAL_DEFINE_BY_NAME),$(if $(filter-out no NO none NONE false FALSE,$($(t))),-D$(t)))
+MTK_INTERNAL_CDEFS += $(foreach t,$(AUTO_ADD_GLOBAL_DEFINE_BY_VALUE),$(if $(filter-out no NO none NONE false FALSE,$($(t))),$(foreach v,$(shell echo $($(t)) | tr '[a-z]' '[A-Z]'),-D$(v))))
+MTK_INTERNAL_CDEFS += $(foreach t,$(AUTO_ADD_GLOBAL_DEFINE_BY_NAME_VALUE),$(if $(filter-out no NO none NONE false FALSE,$($(t))),-D$(t)=\"$(strip $($(t)))\"))
+
+MTK_GLOBAL_CFLAGS += $(MTK_INTERNAL_CDEFS)
+
+ifneq ($(MTK_K64_SUPPORT), yes)
+BOARD_KERNEL_CMDLINE = bootopt=64S3,32S1,32S1
+else
+BOARD_KERNEL_CMDLINE = bootopt=64S3,32N2,64N2
+endif
+
+# ptgen
+MTK_PTGEN_CHIP := $(shell echo $(TARGET_BOARD_PLATFORM) | tr '[a-z]' '[A-Z]')
+-include device/mediatek/build/build/tools/ptgen/$(MTK_PTGEN_CHIP)/ptgen.mk
+
+# Disable product partition
+TARGET_COPY_OUT_PRODUCT := system/product
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE :=
+
+#Config partition size
+-include $(MTK_PTGEN_OUT)/partition_size.mk
+ifneq ($(strip $(MTK_AB_OTA_UPDATER)), yes)
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+endif
+BOARD_FLASH_BLOCK_SIZE := 4096
+BOARD_MTK_EXPDB_SIZE_KB := 20480
+BOARD_MTK_NVCFG_SIZE_KB := 8192
+
+-include device/mediatek/build/core/soong_config.mk
